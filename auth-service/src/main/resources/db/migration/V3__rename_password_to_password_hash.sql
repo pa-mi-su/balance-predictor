99ea82e3e6887e479a0adcc8877825a3d8bdea7f
pa-mi-su <pamisu@protonmail.com>
2025-10-15 20:00:14 -0400

    cleand up secrets and some other small tweaks

--- auth-service/src/main/resources/db/migration/V3__rename_password_to_password_hash.sql
+++
@@ -1,5 +0,0 @@
--- Rename the existing column created by V1
-ALTER TABLE public.users RENAME COLUMN password TO password_hash;
-
--- (optional) tighten length if you want
-ALTER TABLE public.users ALTER COLUMN password_hash TYPE varchar(255);
